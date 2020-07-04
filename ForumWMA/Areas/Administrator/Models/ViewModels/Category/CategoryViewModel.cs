9a40117190b53bafcb8ab70775b6810f3571a10b
GHorozov <georgihorozov@hotmail.com>
2020-07-04 16:49:21 +0300

    Add categories in admin panel, show all categories

---
+++ ForumWMA/Areas/Administrator/Models/ViewModels/Category/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumWMA.Areas.Administrator.Models.ViewModels.Category
+{
+    using ForumWMA.Common.Mapper.Interfaces;
+    using ForumWMA.Data.Models;
+
+    public class CategoryViewModel : IMapFrom<Category>
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
